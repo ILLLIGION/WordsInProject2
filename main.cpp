d70af96bbf2fb96fc2342da41835c38514ba05da
Nikita <dazuk07@yandex.ru>
2016-12-05 10:32:48 +0300

    issue #1 completed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <boost/process.hpp>
 #include <iostream>
 #include <regex>
+#include <exception>
 
 namespace bp = boost::process;
 
@@ -10,21 +11,19 @@ bp::child start_child(std::string exec)
     args.push_back(exec);
     bp::context ctx;
     ctx.stdout_behavior = bp::capture_stream();
-    ctx.stdin_behavior = bp::capture_stream();
     return bp::launch(exec, args, ctx);
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) return 0;
+    if (argc < 2) throw std::invalid_argument("not enough parameters");
 
     bp::child child = start_child(argv[1]);
     bp::pistream &is = child.get_stdout();
-    bp::postream &os = child.get_stdin();
     std::string line;
     std::regex str("\\S+\\s*");
     std::smatch match;
-    int w_counter = 0;
+    unsigned int w_counter = 0;
     while (std::getline(is, line))
     {
         while (std::regex_search(line, match, str))
